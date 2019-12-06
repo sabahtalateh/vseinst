da746c51b0b1b6dae161d419015615c6e99b7aa7
Aleksandr Kravtsov <sabahtalateh@gmail.com>
2019-12-06 22:09:12 +0300

    Fix bin

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-vendor/bin/doctrine orm:schema-tool:drop --force \
-    && vendor/bin/doctrine orm:schema-tool:create \
-    && vendor/bin/phpunit tests --colors --bootstrap src/config/bootstrap.php
+vendor/bin/phpunit tests --colors --bootstrap src/config/bootstrap.php
